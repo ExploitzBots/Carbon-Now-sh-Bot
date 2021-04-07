436f9c7461cd3c4e1e9a8a7ce50f7171e66d78a1
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-04-07 11:50:20 +0530

    Update sample.config.py

--- sample.config.py
+++ sample.config.py
@@ -1,6 +1,6 @@
 import os
 
-class Config:
+class Configs:
     api_id = ""1776579"
     api_hash = "4ef791aa3016d7a53af3a9737a83e381"
     bot_token = "1763068271:AAFWDjNwTtC6glN2Rb0eYnaG1aeqBJVG5_c"
